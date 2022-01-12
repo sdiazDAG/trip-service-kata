709b502ade23dcecb080ecf236f86f33eacfe3db
Santi Diaz <sdiaz@aidacanarias.com>
2022-01-12 08:17:30 +0000

    Create Test to call GetLoguedUser when get trips by user

--- c#/tests/TripServiceKata.Tests/TripServiceShould.cs
+++ c#/tests/TripServiceKata.Tests/TripServiceShould.cs
@@ -1,6 +1,21 @@
-﻿namespace TripServiceKata.Tests
+﻿using System.Collections.Generic;
+using TripServiceKata.Entity;
+using TripServiceKata.Service;
+using Xunit;
+
+namespace TripServiceKata.Tests
 {
     public class TripServiceShould
     {
+        [Fact]
+        public void Call_Get_logued_user()
+        {
+            var tripList = new List<Trip>();
+            var userSession = UserSession.GetInstance();
+            var service = new TripService(tripList, userSession);
+            var user = new User();
+
+            var resultTripList = service.GetTripsByUser(user);
+        }
     }
 }
